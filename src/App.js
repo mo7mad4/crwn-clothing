6e47fea5d68573e5b7476c1686ec55287bd1006c
Mohammed Zeyad Abu Raida <mohammedreda814@gmail.com>
2022-02-19 19:05:43 +0200

    Adding Route and Router for component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Routes } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import { Router } from 'react-router-dom';
+
 import HomePage from './pages/homepage/homepage.components';
 
 function App() {
   return (
   <div>
-    <HomePage />
+    <Routes>
+      <Route exact path='/' element={<HomePage />} />
+    </Routes>
   </div>
   );
 }
